71936c8e55f362bd3f4d9b82fd78ddaa6b112f0c
estefi <alu0100698688@ull.edu.es>
2014-10-24 12:07:13 +0000

    Funcionamiento de la expectativas usando un hash para las opciones de respuesta

--- lib/exam/simpleSelection.rb
+++ lib/exam/simpleSelection.rb
@@ -6,8 +6,16 @@ class SimpleSelection
 		@answers = answers
 	end
 
-	
+	def answers_to_s
+		cadena = ""
+		@answers.each do |m,i| 
+       		cadena += "#{m})#{i}\n"
+      	end
+      	cadena
+	end
 	def to_s
-		"#{@ask}\n "+self.answers_to_s
+		cadena = "#{@ask}\n"
+		cadena +=self.answers_to_s
+		cadena
 	end
 end
--- spec/exam_spec.rb
+++ spec/exam_spec.rb
@@ -3,8 +3,20 @@ require "lib/examen.rb"
 describe Examen do
 
   before :each do
-    @sS1 = SimpleSelection.new("¿De qué color es el coche del presidente?",["azul","verde","negro","naranja"])
-    @sS2 = SimpleSelection.new("¿Que es un perro?",["Un ave","Un reptil","Un mamifero","Ninguna de las anteriores"])
+    @options1 = {
+      "a" => "azul",
+      "b" => "verde",
+      "c" => "negro",
+      "d" => "naranja"
+    }
+    @options2 = {
+      "a" => "Un ave",
+      "b" => "Un reptil",
+      "c" => "Un mamifero",
+      "d" => "Ninguna de las anteriores"
+    }
+    @sS1 = SimpleSelection.new("¿De qué color es el coche del presidente?",@options1)
+    @sS2 = SimpleSelection.new("¿Que es un perro?",@options2)
     @questions =[@sS1,@sS2] 
     @p1 = Examen.new(@questions)
   end  
@@ -16,20 +28,20 @@ describe Examen do
     end
 
     it "Se almacenan correctamente las respuestas." do
-        @sS1.answers_to_s.should eq("a)azul\n b)verde\n c)negro\n d)naranja\n")
+        @sS1.answers_to_s.should eq("a)azul\nb)verde\nc)negro\nd)naranja\n")
     end
   end
   describe "# Obtencion respuestas" do
-    it "Array de opciones" do
-      @sS1.answers.should eq(["azul","verde","negro","naranja"])
+    it "Hash de opciones" do
+      @sS1.answers.should eq({"a"=>"azul", "b"=>"verde", "c"=>"negro", "d"=>"naranja"})
     end
   end 
   describe "#Mostrar preguntas y respuestas" do
     it "Mostrar una pregunta y su respuesta" do
-       @sS1.to_s.should eq("¿De qué color es el coche del presidente?\n a)azul\n b)verde\n c)negro\n d)naranja\n")
+       @sS1.to_s.should eq("¿De qué color es el coche del presidente?\na)azul\nb)verde\nc)negro\nd)naranja\n")
     end  
     it "Preguntas y respuestas de examen" do
-       @p1.to_s.should eq("¿De qué color es el coche del presidente?\n a)azul\n b)verde\n c)negro\n d)naranja\n¿Que es un perro?\n a)Un ave\n b)Un reptil\n c)Un mamifero\n d)Ninguna de las anteriores\n")
+       @p1.to_s.should eq("¿De qué color es el coche del presidente?\na)azul\nb)verde\nc)negro\nd)naranja\n¿Que es un perro?\na)Un ave\nb)Un reptil\nc)Un mamifero\nd)Ninguna de las anteriores\n")
     end
   end
 end
