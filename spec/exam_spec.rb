789a2ec3c82bb7f7d5f10579e7576669bf523381
Adrian <acglez10@gmail.com>
2014-10-24 01:49:06 +0100

    Expectativa creada para el almacenamiento y lectura de pregunta y sus correspondientes respuestas.

--- spec/exam_spec.rb
+++ spec/exam_spec.rb
@@ -2,4 +2,19 @@ require "lib/examen.rb"
 
 describe Examen do
 
+  before :each do
+    @sS = SimpleSelection.new("¿De qué color es el coche del presidente?",["azul","verde","negro","naranja"])
+    @p1 = Examen.new(@sS)
+  end  
+
+  describe "# Almacenamiento de la pregunta." do
+
+    it "Se almacena correctamente la pregunta." do
+        @p1.myQuestion.ask.should eq("¿De qué color es el coche del presidente?")
+    end
+
+    it "Se almacenan correctamente las respuestas." do
+        @p1.myQuestion.answer.should eq("a) azul\nb) verde\nc) negro\nd) naranja\n")
+    end
+  end
 end
