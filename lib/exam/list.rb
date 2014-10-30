01305bdb49d27f262b30e3c825d688e9103c42ce
estefi <alu0100698688@ull.edu.es>
2014-10-30 12:07:42 +0000

    Creado el metodo insertElements para satisfacer la insercion de varios elementos

--- lib/exam/list.rb
+++ lib/exam/list.rb
@@ -18,5 +18,22 @@ class List
        
       actual[:next]
    end
+   def insertElements(arrayNodos)
+      i=0
+      while i<arrayNodos.length
+            actual = @cabeza
+            siguiente = @cabeza[:next]
+
+            while siguiente != nil
+               actual = siguiente
+               siguiente = actual[:next]
+            end
+            actual[:next] = arrayNodos[i]
+            
+            i +=1
+       end
+       actual[:next] #Se devuelve el ultimo nodo insertado
+      
+   end
     
 end
